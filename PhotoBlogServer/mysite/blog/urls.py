a3c1ddcb44c9b5ec67d090a95b75ead5b0648640
twalla26 <ssm4748@naver.com>
2025-10-28 20:13:54 +0900

    Photo blog server

---
+++ PhotoBlogServer/mysite/blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('Post', views.blogImage)
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    # path('js_test/', views.js_test, name='js_test'),
+    path('api_root/', include(router.urls)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ PhotoBlogServer/mysite/blog/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
+from rest_framework import viewsets
+from .serializers import PostSerializer
+from rest_framework import permissions
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+
+class blogImage(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        # API로 POST 요청(create)이 오면,
+        # 토큰을 보낸 사용자(self.request.user)를 'author' 필드에 자동으로 저장
+        serializer.save(author=self.request.user)
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    paginator = Paginator(posts, 5) 
+    page_number = request.GET.get('page')
+    posts = paginator.get_page(page_number)
+
+    # 👈 6. 'posts' 변수를 템플릿에 전달합니다. (변수 이름은 'posts' 그대로)
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect("post_detail", pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required # 로그인한 사용자만 접근 가능
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    
+    if post.author != request.user:
+        return redirect('post_detail', pk=post.pk)
+
+    if request.method == 'POST':
+        post.delete() 
+        return redirect('post_list') 
+    
+    return render(request, 'blog/post_delete_confirm.html', {'post': post})
